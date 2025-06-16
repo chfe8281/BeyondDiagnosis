c691f4f98ddd6c93e560319d39198ec0205ea138
chfe8281 <Christina.Fernandez@colorado.edu>
2025-06-16 14:39:22 -0600

    test

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy.dialects.postgresql import ARRAY
+from datetime import datetime, timezone
 
 from __init__ import db
 
@@ -21,7 +22,7 @@ class Profile(db.Model):
     __tablename__ = 'profiles'
     user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), primary_key=True, nullable = False)
     bio = db.Column(db.String(200))
-    status = db.Column(db.String(50), default = "Patient", nullable=False)
+    status = db.Column(db.String(50), db.CheckConstraint("status IN ('Patient', 'Caregiver')"), default = 'Patient', nullable=False)
     location = db.Column(db.String(50), nullable=False)
     interests = db.Column(ARRAY(db.Text))
     conditions = db.Column(ARRAY(db.Text))
@@ -29,3 +30,11 @@ class Profile(db.Model):
 
     def __repr__(self):
         return f'<Profile user_id={self.user_id}, status={self.status}>'
+    
+class Friend_Requests(db.Model):
+    __tablename__ = 'friend_requests'
+    request_id = db.Column(db.Integer, primary_key = True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
+    receiver_id = db.Column(db.Integer, db.ForeignKey('users.user_id'), nullable = False)
+    requested_at = db.Column(db.DateTime, default = datetime.now(timezone.utc))
+    status = db.Column(db.String(20), db.CheckConstraint("status IN ('pending', 'accepted', 'rejected')"), default = 'pending', nullable = False)
