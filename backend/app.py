6cf7369e45961b08474db86c47cc70e1450a8b1a
chfe8281 <Christina.Fernandez@colorado.edu>
2025-07-30 10:01:47 -0600

    post del func for group owner

--- backend/app.py
+++ backend/app.py
@@ -170,6 +170,8 @@ def dash():
             owner = False
             if post.creator_id == current_user.user_id:
                 owner = True
+            elif group.creator_id == current_user.user_id:
+                owner = True
             dash_posts.append({'post': post, 'group': group, 'creator': user, 'timestamp': timestamp, 'owner': owner})
         
         return render_template('dashboard.html', user = current_user.name, avatar_url = current_user.avatar_url, user_id = current_user.user_id, dash_posts = dash_posts)
