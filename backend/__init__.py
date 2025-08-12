a5fd2403b5c1d1a6bbe39394b3f103eda9050c41
chfe8281 <Christina.Fernandez@colorado.edu>
2025-08-11 18:53:23 -0600

    changed db link in env

--- backend/__init__.py
+++ backend/__init__.py
@@ -10,9 +10,9 @@ load_dotenv()
 fernet_key = os.getenv('FERNET_KEY')
 f = Fernet(fernet_key)
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-print("🔌 Connected to DB:", app.config['SQLALCHEMY_DATABASE_URI'])
+print("🔌 Connected to DB:", app.config['DATABASE_URL'])
 db = SQLAlchemy(app)
 __all__ = ["app", "db", "f"]
