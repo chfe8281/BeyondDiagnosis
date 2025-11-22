4f999ee7d86f8b907add3ff61f027d3c530bc3ae
chfe8281 <Christina.Fernandez@colorado.edu>
2025-11-22 16:01:20 -0700

    changed encoded content length in model

--- backend/models.py
+++ backend/models.py
@@ -101,6 +101,6 @@ class Victories(db.Model):
     victory_id = db.Column(db.Integer, primary_key = True)
     creator_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     created_at = db.Column(db.DateTime(timezone = True), server_default = func.now())
-    content = db.Column(db.String(140), nullable = False)
+    content = db.Column(db.Text, nullable = False)
     likes = db.Column(db.Integer, default = 0, nullable = False)
     anonymous =db.Column(db.Boolean, default = False, nullable = False)
